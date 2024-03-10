76712030a8d569f4c89668ef1c669d4bec04966c
Priyanshu <priyanshu@gmail.com>
2024-03-10 17:42:25 +0530

    update the html, css and Js file

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,35 @@
-const rateButtons = document.querySelectorAll(".rating-btn");
-const rateRange = document.querySelector("#rateRange");
+const rateNum = document.querySelector("#rateNum");
 const popUpBox = document.querySelector("#popUpThanks");
 const rateCard = document.querySelector("#rateCard");
 const subBtn = document.querySelector("#subBtn");
 const closeIcon = document.querySelector("#closeIcon");
+const input = document.querySelectorAll("input");
 
-rateButtons.forEach((button) => {
+let form = document.querySelector("form");
+
+input.forEach((button) => {
   button.addEventListener("click", () => {
-    rateButtons.forEach((btn) => {
+    input.forEach((btn) => {
       btn.style.backgroundColor = null;
       btn.style.color = null;
     });
+
     button.style.backgroundColor = "hsl(216, 12%, 54%)";
     button.style.color = "#ffffff";
 
-    rateRange.innerText = button.innerText;
-  });
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-  subBtn.addEventListener("click", () => {
-    popUpBox.style.display = "block";
-    rateCard.style.display = "none";
-  });
+      popUpBox.style.display = "block";
+      rateCard.style.display = "none";
 
-  closeIcon.addEventListener("click", () => {
-    rateCard.style.display = "block";
-    popUpBox.style.display = "none";
-    rateButtons.forEach((btn) => {
-      btn.style.backgroundColor = null;
-      btn.style.color = null;
     });
+
+    rateNum.innerHTML = button.value;
   });
+});
+
+closeIcon.addEventListener("click", () => {
+  rateCard.style.display = "block";
+  popUpBox.style.display = "none";
 });
